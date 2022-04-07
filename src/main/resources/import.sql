b65e325002c4b688c4652a0c44f8970f3e35981a
Robert David Ramirez Garcia <robert.ramirez.garcia@gmail.com>
2022-04-06 22:31:32 -0500

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES ('PANASONIC',800, NOW())
+INSERT INTO productos (nombre, precio, create_at) VALUES ('SONY', 700, NOW())
+INSERT INTO productos (nombre, precio, create_at) VALUES ('APPLE', 1000, NOW())
