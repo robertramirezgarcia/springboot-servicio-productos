8f8001bc678ed3bab891dfeef3f6c81acda86966
Robert David Ramirez Garcia <robert.ramirez.garcia@gmail.com>
2022-04-15 16:35:27 -0500

    Update controller

--- src/main/java/com/formacionbdi/springboot/app/productos/models/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/models/controllers/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,13 +19,17 @@ public class ProductoController {
 	@Autowired
 	private Environment env;
 
+	@Value("${server.port}")
+	private Integer port;
+
 	@Autowired
 	private IProductoService productoService;
 	
 	@GetMapping("/listar")
 	public List<Producto> listar(){
 		return productoService.findAll().stream().map(producto -> {
-			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
